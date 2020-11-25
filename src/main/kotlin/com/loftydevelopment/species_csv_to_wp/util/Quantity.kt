9361b6fe87e94c097d086bd70785a75f24a73bb3
Connor Loftus <connorlof@gmail.com>
2020-11-24 20:39:33 -0500

    Add AssertJ library, implement Quantity class

--- src/main/kotlin/com/loftydevelopment/species_csv_to_wp/util/Quantity.kt
+++ src/main/kotlin/com/loftydevelopment/species_csv_to_wp/util/Quantity.kt
@@ -1,4 +1,59 @@
 package com.loftydevelopment.species_csv_to_wp.util
 
-class Quantity {
+data class Quantity(
+        val isInfinite: Boolean,
+        val isSingleValueOrGreater: Boolean,
+        val values: List<Int>,
+) {
+    init {
+        if (values.isEmpty()) {
+            require(isInfinite) { "isInfinite must be set if no values are set" }
+        }
+
+        if (isSingleValueOrGreater) {
+            require(values.size == 1) { "Only a single value may be included if isSingleValueOrGreater" }
+        }
+    }
+
+    fun isWithinRange(value: Double): Boolean {
+        if (isInfinite) return true
+
+        if (isSingleValueOrGreater) {
+            return isEqualOrGreater(value)
+        }
+
+        if (containsMatchingValue(value)) {
+            return true
+        }
+
+        return false
+    }
+
+    private fun isEqualOrGreater(value: Double): Boolean {
+        if (values.size > 1 || values.isEmpty()) return false
+
+        if (value >= values[0]) {
+            return true
+        }
+
+        return false
+    }
+
+    private fun containsMatchingValue(value: Double): Boolean {
+        return values.map { it.toDouble() }.contains(value)
+    }
+
+    override fun toString(): String {
+        if (isInfinite) return "any"
+        if (isSingleValueOrGreater) return "${values[0]}+"
+        if (values.size == 1) return "${values[0]}"
+
+        var output = "${values[0]}"
+
+        for (index in 1 until values.size) {
+            output = "${output}, ${values[index]}"
+        }
+
+        return output
+    }
 }
---
+++ src/test/kotlin/com/loftydevelopment/species_csv_to_wp/util/QuantityTest.kt
@@ -0,0 +1,72 @@
+package com.loftydevelopment.species_csv_to_wp.util
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner.StrictStubs
+
+@RunWith(StrictStubs::class)
+class QuantityTest {
+
+    @Test
+    fun requireSingleValueInit() {
+        assertThatThrownBy { createQuantity(isSingleValueOrGreater = true, values = listOf(5, 6)) }
+            .withFailMessage("Only a single value may be included if isSingleValueOrGreater")
+    }
+
+    @Test
+    fun requireInfiniteWhenNoValuesInit() {
+        assertThatThrownBy { createQuantity(isInfinite = false, values = listOf()) }
+            .withFailMessage("isInfinite must be set if no values are set")
+    }
+
+    @Test
+    fun toStringReturnsExpectedValues() {
+        // isInfinite set as true
+        assertThat(createQuantity(isInfinite = true).toString()).isEqualTo("any")
+
+        // isSingleValueOrGreater set as true
+        assertThat(createQuantity(isSingleValueOrGreater = true).toString()).isEqualTo("1+")
+
+        // Single value
+        assertThat(createQuantity().toString()).isEqualTo("1")
+
+        // Multiple values
+        assertThat(createQuantity(values = listOf(1, 2)).toString()).isEqualTo("1, 2")
+        assertThat(createQuantity(values = listOf(1, 2, 4, 5)).toString()).isEqualTo("1, 2, 4, 5")
+    }
+
+    @Test
+    fun isWithinRange() {
+        // Infinite accepts any value
+        val infiniteQuantity = createQuantity(isInfinite = true)
+        assertThat(infiniteQuantity.isWithinRange(0.0)).isTrue
+        assertThat(infiniteQuantity.isWithinRange(-1.0)).isTrue
+        assertThat(infiniteQuantity.isWithinRange(1.0)).isTrue
+        assertThat(infiniteQuantity.isWithinRange(Double.MAX_VALUE)).isTrue
+        assertThat(infiniteQuantity.isWithinRange(Double.MIN_VALUE)).isTrue
+
+        // Single value or greater accepts above, equal, but not below
+        val singleValueGreaterThan = createQuantity(isSingleValueOrGreater = true, values = listOf(5))
+        assertThat(singleValueGreaterThan.isWithinRange(0.0)).isFalse
+        assertThat(singleValueGreaterThan.isWithinRange(4.0)).isFalse
+        assertThat(singleValueGreaterThan.isWithinRange(5.0)).isTrue
+        assertThat(singleValueGreaterThan.isWithinRange(6.0)).isTrue
+
+        // Multiple values accepts either but not another value
+        val multipleValues = createQuantity(values = listOf(2, 4, 6))
+        assertThat(multipleValues.isWithinRange(1.0)).isFalse
+        assertThat(multipleValues.isWithinRange(7.0)).isFalse
+        assertThat(multipleValues.isWithinRange(2.0)).isTrue
+        assertThat(multipleValues.isWithinRange(4.0)).isTrue
+        assertThat(multipleValues.isWithinRange(6.0)).isTrue
+    }
+
+    private fun createQuantity(
+            isInfinite: Boolean = false,
+            isSingleValueOrGreater: Boolean = false,
+            values: List<Int> = listOf(1),
+    ) = Quantity(isInfinite = isInfinite, isSingleValueOrGreater = isSingleValueOrGreater, values = values)
+
+}
